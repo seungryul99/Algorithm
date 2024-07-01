b9a1cfc10dea14687b7432eac43e3c7ae99f1776
백엔드 호소인 <100277876+seungryul99@users.noreply.github.com>
2024-07-01 12:19:43 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        
+        int sum = 0;
+        
+        for (int i=0;i<absolutes.length;i++){
+            
+            if (signs[i] == false) absolutes[i] *= -1;
+            sum += absolutes[i];
+        }
+        
+        return sum;
+    }
+}
