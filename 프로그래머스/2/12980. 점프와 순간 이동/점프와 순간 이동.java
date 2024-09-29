78e8da57ffeef27cbe7989f855d877c786543077
백엔드 호소인 <100277876+seungryul99@users.noreply.github.com>
2024-09-29 19:54:29 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        
+        int cnt = 0;
+
+        while(n>0){
+            
+            if (n % 2==0) n /= 2;
+            else {
+                cnt ++;
+                n--;
+            }
+        }
+        
+        return cnt;
+    }
+}
