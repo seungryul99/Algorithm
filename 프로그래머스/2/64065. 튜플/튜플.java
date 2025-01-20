2ff9eebfcad374a1f4f620ea54b26df03b6af3eb
SeungRyul <100277876+seungryul99@users.noreply.github.com>
2025-01-20 14:44:19 +0900

    [level 2] Title: 튜플, Time: 85.86 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -3,9 +3,11 @@ import java.util.*;
 class Solution {
     public Set<Integer> solution(String s) {
         
+        // {} 제거,   ex)    2   ,   2,1   ,   2,1,3  ,    2,1,3,4
         StringTokenizer stk = new StringTokenizer(s,"{}");
         List<String> list = new ArrayList<>();
         
+        // 부분집합간 ',' 제거,     ex) 2   2,1    2,1,3     2,1,3,4
         while(stk.hasMoreTokens()){
             
             String cur = stk.nextToken();
@@ -14,8 +16,11 @@ class Solution {
             list.add(cur);
         }
         
+        // List에 넣고 String의 길이 기준으로 sort
         list.sort(Comparator.comparingInt(String::length));
         
+        
+        // 입력 순서가 보장되는 LinkedHashSet 사용
         HashSet<Integer> st = new LinkedHashSet<>();
         
         for(int i=0; i<list.size(); i++){
