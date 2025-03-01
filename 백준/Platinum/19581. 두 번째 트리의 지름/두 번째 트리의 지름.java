bfaaff56f61990cff31692148ab78b6be3242a19
SeungRyul <100277876+seungryul99@users.noreply.github.com>
2025-03-01 11:10:54 +0900

    [Platinum V] Title: 두 번째 트리의 지름, Time: 632 ms, Memory: 70920 KB -BaekjoonHub

---
+++ 백준/Platinum/19581. 두 번째 트리의 지름/두 번째 트리의 지름.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static class Node{
+        int to;
+        int cost;
+
+        public Node(int to, int cost){
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+
+    static int n, maxNode, max;
+    static boolean[] visited;
+    static List<Node>[] adj;
+
+    static void dfs(int cur, int sum){
+
+        int cnt = 0;
+        visited[cur]= true;
+
+        for (Node next : adj[cur]){
+
+            if(visited[next.to]) continue;
+            dfs(next.to, sum + next.cost);
+            cnt ++ ;
+        }
+
+        if (cnt == 0 && max < sum){
+            maxNode = cur;
+            max = sum;
+        }
+    }
+
+    static void init(){
+        max = 0;
+        maxNode=0;
+        Arrays.fill(visited,false);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        adj = new List[n+1];
+
+        for (int i=1; i<=n; i++) adj[i] = new ArrayList<>();
+
+        visited = new boolean[n+1];
+
+        for (int i=0; i< n-1; i++){
+
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+
+            int from = Integer.parseInt(stk.nextToken());
+            int to = Integer.parseInt(stk.nextToken());
+            int cost = Integer.parseInt(stk.nextToken());
+
+            adj[from].add(new Node(to, cost));
+            adj[to].add(new Node(from, cost));
+        }
+
+        dfs(1,0);
+        int a = maxNode;
+        init();
+
+        dfs(a,0);
+        int b = maxNode;
+        init();
+
+        visited[b] = true;
+        dfs(a,0);
+        int res = max;
+        init();
+
+        visited[a] = true;
+        dfs(b,0);
+
+        System.out.println(Math.max(max, res));
+
+    }
+}
