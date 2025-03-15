12274afe5c0519b2c1ae9fb5980555ecf2c41c6e
KimSeungRyul <100277876+seungryul99@users.noreply.github.com>
2025-03-15 20:35:17 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 1, Time: 568 ms, Memory: 17500 KB -BaekjoonHub

---
+++ 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(stk.nextToken());
+        int k = Integer.parseInt(stk.nextToken());
+
+        stk = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for(int i=0 ;i<n; i++) arr[i] = Integer.parseInt(stk.nextToken());
+
+        int cnt = 0;
+
+        for (int i=n-1; i>0 ; i--){
+
+            for (int j=0; j<n-1; j++){
+
+                if(arr[j] > arr[j+1]){
+
+                    int tmp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = tmp;
+
+                    cnt++;
+
+                    if(k==cnt){
+
+                        System.out.print(arr[j] + " " + arr[j+1]);
+                    }
+                }
+            }
+        }
+
+        if (cnt < k) System.out.println(-1);
+    }
+}
