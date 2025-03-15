6e812718327651bc19f750efd9154ff56c6f0cf1
KimSeungRyul <100277876+seungryul99@users.noreply.github.com>
2025-03-15 20:23:51 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 292 ms, Memory: 18396 KB -BaekjoonHub

---
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(stk.nextToken());
+        int k = Integer.parseInt(stk.nextToken());
+
+        stk = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for(int i=0 ;i<n; i++) arr[i] = Integer.parseInt(stk.nextToken());
+
+        int res1 = 0, res2 =0;
+        int cnt = 0;
+
+        for (int last = n-1; last > 0; last--){
+
+            int max = -1;
+            int pos = -1;
+            for (int i=0; i<=last; i++){
+
+                if (max < arr[i]){
+
+                    pos = i;
+                    max = arr[i];
+                }
+            }
+
+            if (pos != last) {
+
+                int temp = arr[last];
+                arr[last] = arr[pos];
+                arr[pos] = temp;
+                cnt ++;
+
+                if (cnt == k) {
+                    res1 = arr[pos];
+                    res2 = arr[last];
+                }
+            }
+        }
+
+        if (cnt < k) System.out.println(-1);
+        else System.out.println(res1 + " " + res2);
+
+    }
+}
