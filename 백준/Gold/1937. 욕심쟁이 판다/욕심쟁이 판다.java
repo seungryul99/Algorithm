b18bd1c15509a57e9bd5ba8900f9e020571429c0
KimSeungRyul <100277876+seungryul99@users.noreply.github.com>
2025-07-12 15:17:39 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 464 ms, Memory: 37208 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -0,0 +1,69 @@
+import static java.lang.Math.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[][] adj;
+    static int[][] dp;
+    static int n;
+    static int[] dy = {1,0,-1,0};
+    static int[] dx = {0,1,0,-1};
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(stk.nextToken());
+        adj = new int[n][n];
+        dp = new int[n][n];
+
+        for (int i=0; i<n; i++) {
+            stk = new StringTokenizer(br.readLine());
+
+            for (int j=0; j<n; j++) {
+
+                adj[i][j] = Integer.parseInt(stk.nextToken());
+            }
+        }
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        int res = 0;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                res = max(go(i,j), res);
+            }
+        }
+
+        System.out.println(res);
+    }
+
+
+    public static int go(int y, int x) {
+
+        if(dp[y][x] != -1) return dp[y][x];
+        dp[y][x] = 1;
+
+
+        for (int w=0; w<4; w++) {
+            int ny = y + dy[w];
+            int nx = x + dx[w];
+
+            if(ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+            if(adj[ny][nx] <= adj[y][x]) continue;
+
+            dp[y][x] = max(dp[y][x], 1 + go(ny, nx));
+        }
+
+        return dp[y][x];
+    }
+}
