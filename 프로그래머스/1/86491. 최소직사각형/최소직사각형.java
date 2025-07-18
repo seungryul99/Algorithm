b1b8ca0e1d8c2bc8bb06bd12b4981aa8d92975e1
Ryul99 <100277876+seungryul99@users.noreply.github.com>
2025-07-18 19:06:04 +0900

    [level 1] Title: 최소직사각형, Time: 1.69 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,22 +1,34 @@
+import java.util.*;
+
 class Solution {
-    public int solution(int[][] sizes) {
-       
-        int answer = 1;
+    
+    public static int lastIndex;
+    public static int[][] sizes;
+    
+    public int solution(int[][] input) {
+    
+        sizes = new int[input.length][2];
         
-        int len = sizes.length;
-        int width = 0;
-        int length = 0;
-        
-        // 가로 구함
-        for (int i=0; i<len; i++){
+        for (int i=0; i<input.length; i++){
             
-            int curWidth = sizes[i][0];
-            int curLength = sizes[i][1];
-            
-            width = Math.max(width, Math.max(curWidth, curLength));
-            length = Math.max(length, Math.min(curWidth, curLength));
+            for (int j=0; j<2; j++) sizes[i][j] = input[i][j];
         }
+
+        lastIndex = input.length -1;
         
-        return width * length;
+        return go(0, 0, 0);
+    }
+    
+    public static int go(int index, int maxWidth, int maxLength){
+        
+        if (index == lastIndex + 1) return maxWidth * maxLength;
+    
+        int curWidth = sizes[index][0];
+        int curLength = sizes[index][1];
+        
+        int val = go(index + 1, Math.max(maxWidth, curWidth), Math.max(maxLength, curLength));
+        val = Math.min(val, go(index + 1, Math.max(maxWidth, curLength), Math.max(maxLength, curWidth)));
+        
+        return val;
     }
 }
