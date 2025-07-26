5772737634607dd4a7de6b69ec3ffaaddc38b3c0
Ryul99 <100277876+seungryul99@users.noreply.github.com>
2025-07-27 03:08:55 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.21 ms, Memory: 89.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+class Solution {
+    
+    static class Pair{
+        int y, x;
+        
+        public Pair(int y, int x){
+            this.y=y;
+            this.x=x;
+        }
+    }
+    
+    static Map<Integer, Pair> mp = new HashMap<>();
+  
+    public String solution(int[] numbers, String hand) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        int num = 1;
+        for (int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                
+                mp.put(num++, new Pair(i,j));
+            }
+        }
+        
+        mp.put(0, new Pair(3,1));
+        mp.put(-1, new Pair(3,0));  // *
+        mp.put(-2, new Pair(3,2));  // #
+        
+        int left = -1; 
+        int right = -2; 
+       
+        for (int cur : numbers){
+
+            if (cur == 1 || cur == 4 || cur == 7){
+                
+                left = cur;
+                sb.append("L");
+            }
+
+            else if (cur == 3 || cur == 6 || cur == 9){
+
+                right = cur;
+                sb.append("R");
+            }
+
+            else {
+                
+                int leftDist = dist(left, cur);
+                int rightDist = dist(right, cur);
+                
+                if(leftDist < rightDist){
+                    
+                    left = cur;
+                    sb.append("L");
+                }
+                else if (leftDist > rightDist){
+                    
+                    right = cur;
+                    sb.append("R");
+                }
+                else {
+                    
+                    if(hand.equals("left")){
+                        
+                        left = cur;
+                        sb.append("L");
+                    }
+                    else {
+                        
+                        right = cur;
+                        sb.append("R");
+                    }
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+    
+    // 실제 거리까지 구할 필요는 없어보임, 가중치만
+    public static int dist(int dir, int cur){
+        
+        int dirY = mp.get(dir).y;
+        int dirX = mp.get(dir).x;
+        int curY = mp.get(cur).y;
+        int curX = mp.get(cur).x;
+        
+        return Math.abs(dirY-curY)+ Math.abs(dirX-curX);
+    }
+}
