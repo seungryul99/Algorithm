d802f5deb126686a1d495817bccb89206c83c064
Ryul99 <100277876+seungryul99@users.noreply.github.com>
2025-07-29 12:07:18 +0900

    [level 1] Title: 실패율, Time: 5.75 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,46 +1,48 @@
 import java.util.*;
 
-class Pair implements Comparable<Pair>{
-        
-    int stage;
-    double fail;
-        
-    public Pair(int stage, double fail){
-        this.stage = stage;
-        this.fail = fail;
-    }
-     
-    @Override
-    public int compareTo(Pair p) {
-        
-        if (this.fail < p.fail) return 1;
-        if (this.fail > p.fail) return -1;
-        
-        return this.stage < p.stage ? -1 : 1;
-    }
-    
-}
-
 class Solution {
+    
+    static class Stage implements Comparable<Stage>{
+        
+        int num;
+        double success;
+        
+        public Stage(int num, double success){
+            this.num = num;
+            this.success = success;
+        }
+        
+        @Override
+        public int compareTo(Stage otherStage){
+            
+            if (this.success < otherStage.success) return 1;
+            else if (this.success > otherStage.success) return -1;
+            else {
+                return this.num < otherStage.num ? -1 : 1;
+            }
+        }
+    }
     
     public int[] solution(int N, int[] stages) {
         
-        // cnt 배열로 각 개수 카운팅 + 누적합 계산
         int[] cnt = new int[N+2];
         int[] psum = new int[N+2];
+        Set<Stage> st = new TreeSet<>();
         
-        for (int e : stages) cnt[e]++;
-        for (int i=1; i<=N+1; i++) psum[i] = psum[i-1] + cnt[i];
+        for (int x : stages) cnt[x]++;
         
-        // 각 스테이지의 실패율 계산
-        List<Pair> failList = new ArrayList<>();
-        for (int i=1; i<=N; i++) failList.add(new Pair(i,(double)cnt[i] / (psum[N+1] - psum[i-1])));
+        psum[N+1] = cnt[N+1];
+        for (int i=N; i>=1; i--) psum[i] = psum[i+1] + cnt[i]; 
         
-        // 정렬
-        failList.sort(Comparator.naturalOrder());
+        for (int i=1; i<=N; i++){
+            
+            double success = (double) cnt[i] / psum[i];
+            st.add(new Stage(i, success));
+        }
         
         int[] res = new int[N];
-        for(int i=0; i<failList.size();i++) res[i] = failList.get(i).stage;
+        int idx = 0;
+        for (Stage s : st) res[idx++] = s.num; 
         
         return res;
     }
