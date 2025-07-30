ad66202ca023686a657aa82dfd6a090e30af6c33
Ryul99 <100277876+seungryul99@users.noreply.github.com>
2025-07-30 14:47:18 +0900

    [level 1] Title: [1차] 비밀지도, Time: 8.83 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -4,26 +4,11 @@ class Solution {
         String[] bit = new String[n];
         for (int i=0; i<n; i++) bit[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
         
-        // for(String s : bit) {
-        //     s = s.replace("0", " ");
-        //     s = s.replace("1", "#");
-        // }
-        
         for (int i=0; i<n; i++){
             
-            bit[i] = bit[i].replace("0", " ").replace("1", "#");
-            bit[i] = prefix(n, bit[i]);
+            bit[i] = String.format("%"+n+"s", bit[i].replace("0", " ").replace("1", "#"));
         }
         
         return bit;
-    }
-    
-    public String prefix(int n, String s){
-        
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i=0; i<n - s.length(); i++) sb.append(" ");
-        
-        return sb.toString() + s;
     }
 }
