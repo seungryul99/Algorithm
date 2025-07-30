137fbb22a36f1871dae6f28b2fdba6ca764e0468
Ryul99 <100277876+seungryul99@users.noreply.github.com>
2025-07-30 14:07:01 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.04 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,48 +1,50 @@
+import java.util.*;
+
 class Solution {
+
     public int solution(String dartResult) {
         
-        int len = dartResult.length();
         int[] score = new int[3];
         
-     
-        int idx=0;
-        for (int i=0; i<len; i++){
+        int idx = -1;
+        for (int i=0; i<dartResult.length(); i++){
             
-            char c = dartResult.charAt(i);
+            char cur = dartResult.charAt(i);
             
-            if(c >= '0' && c <= '9'){
+            if(cur >= '0' && cur <= '9'){
                 
-                if (c=='0') continue;
-                else if (c=='1') {
-                    score[idx] = 1;
-                    if (dartResult.charAt(i+1) == '0') score[idx] *= 10;
-                }
-                else score[idx] = (int) c - 48;
-            }
-            
-            else if(c == 'S') {
-                if(i<len-1 && dartResult.charAt(i+1) != '*' && dartResult.charAt(i+1) != '#') idx++;   
-            }
-            else if(c == 'D') {
-                score[idx] *= score[idx];
-                if(i<len-1 && dartResult.charAt(i+1) != '*' && dartResult.charAt(i+1) != '#') idx++;   
-            }
-            else if(c == 'T') {
-                score[idx] *= score[idx] * score[idx];
-                if(i<len-1 && dartResult.charAt(i+1) != '*' && dartResult.charAt(i+1) != '#') idx++;   
-            }
-            else if(c == '*') {
-                
-                if (idx == 0) score[idx] *= 2;
-                else {
-                    score[idx] *= 2;
-                    score[idx-1] *= 2;
-                }
                 idx++;
+                
+                if (cur == '1' && dartResult.charAt(i+1) == '0') {
+                    score[idx] = 10;
+                    i++;
+                }
+                else score[idx] = Integer.valueOf(cur - '0');
             }
-            else if(c == '#') score[idx++] *= -1;
+            
+            else if(cur == 'S' || cur == 'D' || cur == 'T'){
+                
+                if (cur == 'T') score[idx]  *= score[idx] * score[idx];
+                else if (cur == 'D') score[idx] *= score[idx];
+            }
+            
+            else {
+                
+                if(cur == '*') {
+                    
+                    if(idx == 0) score[idx] *= 2;
+                    else {
+                        
+                        score[idx] *= 2;
+                        score[idx -1] *= 2;
+                    }
+                }
+                else if(cur == '#') score[idx] *= -1;
+            }
         }
         
-        return score[0] + score[1] + score[2];
+        
+        int res = score[0] + score[1] + score[2];
+        return res;
     }
 }
