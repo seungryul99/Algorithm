96f2bdc8b0e7a08d768debc350f265cdcb2b1e2b
Ryul99 <100277876+seungryul99@users.noreply.github.com>
2025-07-30 14:52:17 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.03 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -2,13 +2,11 @@ class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         
         String[] bit = new String[n];
-        for (int i=0; i<n; i++) bit[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-        
-        for (int i=0; i<n; i++){
-            
-            bit[i] = String.format("%"+n+"s", bit[i].replace("0", " ").replace("1", "#"));
-        }
-        
+        for (int i=0; i<n; i++) bit[i] = String.format("%"+n+"s",Integer.toBinaryString(arr1[i] | arr2[i])
+                                                                        .replace("0", " ")
+                                                                        .replace("1", "#")
+                                                      );
+      
         return bit;
     }
 }
