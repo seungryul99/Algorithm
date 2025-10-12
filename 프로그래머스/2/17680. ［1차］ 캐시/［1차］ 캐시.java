3810abbca3aed1e605ffbc1e9bc3000426f4acca
Ryul99 <100277876+seungryul99@users.noreply.github.com>
2025-10-12 20:42:56 +0900

    [level 2] Title: [1차] 캐시, Time: 116.04 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,30 +3,52 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         
-        if (cacheSize==0) return cities.length * 5;
+        Map<String, Integer> cache = new HashMap<>();
+        int res = 0, time = 1;
         
-        int res = 0;
-        List<String> cache = new ArrayList<>();
-        String tmp;
+        if (cacheSize == 0) return cities.length * 5;
         
-        for(String city : cities){
+        for (String city : cities) {
             
-            tmp = city.toLowerCase();
+            city = city.toLowerCase();
             
-            if(cache.contains(tmp)){
+            int now = cache.getOrDefault(city, 0);
+            
+            // cache miss
+            if (now == 0){
                 
-                cache.remove(tmp);
-                cache.add(tmp);
-                res++;
+                if(cache.size() < cacheSize) {
+                    cache.put(city, time);
+                }
+                else {
+                    
+                    int min = 10000000;
+                    String deleteKey="";
+                    for(String key : cache.keySet()){
+                        
+                        int val = cache.get(key);
+                        
+                        if (min > val){
+                            min = val;
+                            deleteKey = key;
+                        }
+                    }
+                    
+                    cache.remove(deleteKey);
+                    cache.put(city, time);
+                }
+                res += 5;
             }
-            else{
-
-                if(cacheSize == cache.size()) cache.remove(0);               
-                cache.add(tmp);
-                res+=5;
+            
+            // cache hit
+            else {
+                cache.put(city, time);
+                res += 1;
             }
+            
+            time++;
         }
-        
+
         return res;
     }
 }
