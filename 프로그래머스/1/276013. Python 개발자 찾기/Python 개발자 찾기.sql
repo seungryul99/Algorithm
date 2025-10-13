8760e2bce5e24637e8f905c2e1a234818b0ff391
Ryul99 <100277876+seungryul99@users.noreply.github.com>
2025-10-13 21:29:37 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,12 +1,6 @@
-SELECT ID,
-       EMAIL,
-       FIRST_NAME,
-       LAST_NAME
-
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME 
 FROM DEVELOPER_INFOS 
-
-WHERE SKILL_1 = 'Python' OR
-      SKILL_2 = 'Python' OR
+WHERE SKILL_1 = 'Python' or 
+      SKILL_2 = 'Python' or
       SKILL_3 = 'Python'
-      
-ORDER BY ID
+Order By id
