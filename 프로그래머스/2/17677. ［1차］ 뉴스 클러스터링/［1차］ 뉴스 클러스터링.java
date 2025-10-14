f7974af53a9197930e7f1d1fb1cace94b79b1896
Ryul99 <100277876+seungryul99@users.noreply.github.com>
2025-10-14 20:11:54 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.88 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,74 +1,63 @@
 import java.util.*;
 
 class Solution {
-    
-    static void convertMap(Map<String,Integer> mp, String str){
-        
-        String sub = null;
-        
-        for (int i=0; i<str.length()-1; i++){
-            
-            sub = str.substring(i,i+2);
-        
-            if(sub.charAt(0) >= 'a' && sub.charAt(0) <= 'z' && sub.charAt(1) >= 'a' && sub.charAt(1) <= 'z') {
-                if(mp.containsKey(sub)) mp.put(sub, mp.get(sub)+1);
-                else mp.put(sub,1);
-            }
-        }
-    }
-    
     public int solution(String str1, String str2) {
-      
-        // 모두 소문자로 변환
-        str1 = str1.toLowerCase();
-        str2 = str2.toLowerCase();
         
-        // 다중집합 원소 카운팅 맵으로 변환
-        Map<String,Integer> mp1 = new HashMap<>();
-        Map<String,Integer> mp2 = new HashMap<>();
-        convertMap(mp1, str1);
-        convertMap(mp2, str2);
+        str1 = str1.toUpperCase();
+        str2 = str2.toUpperCase();
         
-        // [flow]
-        // 1. 집합 A를 기준으로 집합 B 비교 하면서 겹치는 Key 탐색 후 Union, InterSection 계산
-        // 2. 집합 B를 기준으로 남은 모든 원소를 Union에 합함
-    
-        int union = 0, intersection = 0;
-        List<String> checked = new ArrayList<>();
+        Map<String, Integer> mp1 = new HashMap<>();
+        Map<String, Integer> mp2 = new HashMap<>();
         
-        for(String e1 : mp1.keySet()){
+        for (int i=0; i<str1.length()-1; i++){
             
-            if(mp2.containsKey(e1)){
+            String key = str1.substring(i,i+2);
+            
+            if(key.charAt(0) < 'A' || key.charAt(0) > 'Z') continue;
+            if(key.charAt(1) < 'A' || key.charAt(1) > 'Z') continue;
+            
+            Integer val = mp1.getOrDefault(key, 0);
+            mp1.put(key, val + 1);
+        }
+        
+        for (int i=0; i<str2.length()-1; i++){
+            
+            String key = str2.substring(i,i+2);
+            
+            if(key.charAt(0) < 'A' || key.charAt(0) > 'Z') continue;
+            if(key.charAt(1) < 'A' || key.charAt(1) > 'Z') continue;
+            
+            Integer val = mp2.getOrDefault(key, 0);
+            mp2.put(key, val + 1);
+        }
+        
+        if(mp1.size() == 0 && mp2.size()==0) return 65536;
+        
+        int inter = 0, union = 0;
+        
+        for (String key1 : mp1.keySet()){
+           
+            Set<String> key2Set = mp2.keySet();
+            
+            if(key2Set.contains(key1)){
                 
-                checked.add(e1);
+                int val1 = mp1.get(key1);
+                int val2 = mp2.get(key1);
                 
-                int value1 = mp1.get(e1);
-                int value2 = mp2.get(e1);
-                
-                if (value1 == value2) {
-                    union += value1;
-                    intersection += value1;
-                }
-                else if (value1 > value2){
-                    union += value1;
-                    intersection += value2;
-                }
-                else {
-                    union += value2;
-                    intersection += value1;
-                }
+                inter += val1 > val2 ? val2 : val1;
+                union += val1 > val2 ? val1 : val2;
             }
-            
-            else union += mp1.get(e1);
+            else union += mp1.get(key1);
         }
         
-        for(String e2 : mp2.keySet()){
+        for (String key2 : mp2.keySet()){
             
-            if(checked.contains(e2)) continue;
-            union += mp2.get(e2);
+            Set<String> key1Set = mp1.keySet();
+            
+            if(key1Set.contains(key2)) continue;
+            else union += mp2.get(key2);
         }
         
-        if(union == 0 && intersection ==0) return 65536;
-        else return (int)((double)intersection/union*65536);
+        return (int)(((double)inter / union) * 65536);
     }
 }
